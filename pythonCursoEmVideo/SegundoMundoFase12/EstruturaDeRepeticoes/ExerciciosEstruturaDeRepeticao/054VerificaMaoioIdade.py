d16ac01021e89daeeba9da1be886dd1b1215b151
Reginaldo <florentinolim@gmail.com>
2021-09-11 19:21:38 -0300

    fializando execicio maior idade

--- pythonCursoEmVideo/SegundoMundoFase12/EstruturaDeRepeticoes/ExerciciosEstruturaDeRepeticao/054VerificaMaoioIdade.py
+++ pythonCursoEmVideo/SegundoMundoFase12/EstruturaDeRepeticoes/ExerciciosEstruturaDeRepeticao/054VerificaMaoioIdade.py
@@ -2,6 +2,7 @@
 # No final mostre quantas pessoas ainda não atigiram a maior idade
 #E quantos já são maiores
 s = 0
+s1 = 0
 from datetime import date
 for n in range(0, 7):
     AnoNasc = int(input('Digite o ano de nascimento: '))
@@ -12,6 +13,8 @@ for n in range(0, 7):
         s += p
         print('Atingiu a MAIOR IDADE, você tem {} anos de idade'.format(IdadeAtual))
     elif IdadeAtual < 21:
+        p = 1
+        s1 += p
         print('Não atingiu a MAIOR IDADE, você tem {} anos de idade'.format(IdadeAtual))
 print('Numero de pessoas MAIOR IDADE SÃO: {}'.format(s))
-print('Numero de pessoas MENOR IDADE SÃO: {}'.format(s))
+print('Numero de pessoas MENOR IDADE SÃO: {}'.format(s1))
