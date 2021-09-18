5d638dd84c52c20797d30af8980348145d9e0176
Reginaldo Florentino Lima <reginaldo.lima@ccee.org.br>
2021-09-18 19:17:52 -0300

    messages

--- pythonCursoEmVideo/SegundoMundoFase12/EstruturaDeRepeticoes/ExerciciosEstruturaDeRepeticao/055VerificaMaioEMenorPeso.py
+++ pythonCursoEmVideo/SegundoMundoFase12/EstruturaDeRepeticoes/ExerciciosEstruturaDeRepeticao/055VerificaMaioEMenorPeso.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia o peso de 5 pessoas.
 # No final, mostre qual foi o maior e o menor pesos lidos.
-for p in range(0, 3):
-    peso = int(input('Digite o peso do malandro: Kg '))
+for p in range(1, 4):
+    peso = int(input('Digite o peso do {}ºmalandro: Kg '.format(p)))
     print(peso)
 
