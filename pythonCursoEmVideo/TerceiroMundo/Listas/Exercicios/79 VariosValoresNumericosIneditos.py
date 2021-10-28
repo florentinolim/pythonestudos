96e43d80141af1db8c88628f0a339f705b374945
Reginaldo Florentino Lima <reginaldo.lima@ccee.org.br>
2021-10-28 14:18:41 -0300

    ExerciosListas

---
+++ pythonCursoEmVideo/TerceiroMundo/Listas/Exercicios/79 VariosValoresNumericosIneditos.py
@@ -0,0 +1,3 @@
+'''Crie um programa onde o usuário possa digitar varios valores numéricos e cadastre em uma lista.
+Caso o numero já exista, ele não será adicionado.No final, serão exibidos todos os valores únicos digitados
+em ordem cescente.'''
