36f783b7436e45a9fe9c0d0407c5f4cf8dab1308
Reginaldo Florentino Lima <reginaldo.lima@ccee.org.br>
2021-10-28 14:19:26 -0300

    ExerciosListas

---
+++ pythonCursoEmVideo/TerceiroMundo/Listas/Exercicios/80 LerCincoValoresNaposicaoDaLista.py
@@ -0,0 +1,3 @@
+'''Crie um programa onde o usúário possa digitar cinco valores númericos e cadastre-os em uma posição
+da lista, já na posição correta de inserção(Sem usar o srt()).
+No final mostre a lista ordenada'''
---
+++ pythonCursoEmVideo/TerceiroMundo/Listas/Exercicios/81 LerVariosNumerosColocarNalista.py
@@ -0,0 +1,6 @@
+'''Crie um programa que vai ler vários números e colocar em uma lista.
+Depois mostre:
+A) Quantos numeros foram digitados.
+B) A lista de valores, ordenada de forma descrescente
+C) Se o valor 5 foi digitado e está ou não na lista.
+'''
